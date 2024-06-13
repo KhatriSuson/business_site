daabc8c37b4df777a2d096bc9788ea6b04af680d
KhatriSuson <susonkhatri110@gmail.com>
2024-06-13 12:33:06 +0545

    templates

--- business_portal/business_portal/settings.py
+++ business_portal/business_portal/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'business_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
