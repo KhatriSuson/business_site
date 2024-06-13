1ccfde3ec9018f2a102513f064837c0316ee4c9c
KhatriSuson <susonkhatri110@gmail.com>
2024-06-13 14:42:28 +0545

    initial commit

--- business_portal/businessapp/views.py
+++ business_portal/businessapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Navbar
 # Create your views here.
 
 def home(request):
+    views = {}
+    views["navbar"]= Navbar.objects.all()
     return render(request, 'index.html')
